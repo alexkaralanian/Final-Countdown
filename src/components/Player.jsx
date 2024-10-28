3694d68dbdd6531ef9b8bbe2e7245c3367ac831c
Alex Karalanian <alex.karalanian@gmail.com>
2024-10-28 16:57:17 -0400

    Added Basic Controlled Form Component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(evt) {
+    setSubmitted(false);
+    setPlayerName(evt.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted && playerName ? playerName : 'Unknown Entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={handleChange} value={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
