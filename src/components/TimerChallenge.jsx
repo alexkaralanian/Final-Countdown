4d54ed0805c85694af24fcf669866eaed0141101
Alex Karalanian <alex.karalanian@gmail.com>
2024-10-29 15:49:36 -0400

    Added ref to manager timers

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -4,18 +4,24 @@ export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
-  let timer;
+  // Each component instance will get its own timer ref that works independelty from toher refs of other comonent instances
+  // This ref will not be reset or cleared when component re-executes
+  // React stores the values and makes sure that refs dont get cleared, like state values
+  // Unlike state values, updating refs doesn't trigger a component re-render.
+  // Useful for values that dont directly impact UI but that we need to track over compponent lifecycle
+
+  const timer = useRef();
 
   function handleStart() {
     setTimerStarted(true);
 
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
   }
 
   function handleStop() {
-    clearTimeout(timer);
+    clearTimeout(timer.current);
   }
 
   return (
